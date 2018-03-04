d5f45faa2bae3abc0aa380996bcacd43ea750d95
Brady <vps@bao3.org>
2018-03-04 19:01:52 +0800

    Sun Mar  4 19:01:52 CST 2018

--- atv/update.sh
+++ atv/update.sh
@@ -2,6 +2,6 @@ rm Packages
 rm Packages.gz
 rm Packages.bz2
 dpkg-scanpackages ./debs /dev/null > Packages
-dpkg-scanpackages -m . | gzip -c > Packages.gz
+#dpkg-scanpackages -m . | gzip -c > Packages.gz
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
